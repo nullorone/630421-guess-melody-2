5c4a5387ec30332f5e3af4d4f5437655a764670f
nullorone <sy.v@mail.ru>
2019-10-19 17:30:14 +0300

    Организовывает проброс свойств в экран приветствия

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import {Welcome} from '../welcome/welcome';
+import {getRandomNumber} from "../../util";
+
+const GAME_TIMES = [5, 8, 10];
+const AMOUNT_MISTAKES = [0, 3, 5];
+
+const randomGameTime = getRandomNumber(0, GAME_TIMES.length - 1);
+const randomAmountMistakes = getRandomNumber(0, AMOUNT_MISTAKES.length - 1);
 
 const App = () => {
-  return <Welcome />;
+  return (
+    <Welcome
+      gameTime={GAME_TIMES[randomGameTime]}
+      amountMistakes={AMOUNT_MISTAKES[randomAmountMistakes]}
+    />);
 };
 
 export {App};
