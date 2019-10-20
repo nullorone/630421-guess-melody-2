d3dd2798829d177d8ec854133bd99d10267c4c4f
nullorone <sy.v@mail.ru>
2019-10-20 13:32:28 +0300

    Добавляет интеграционные тесты

---
+++ src/components/welcome/welcome.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Welcome} from "./welcome";
+
+it(`Render a greeting`, () => {
+  const tree = renderer
+    .create(
+        <Welcome
+          gameTime={0}
+          amountMistakes={0}
+        />
+    );
+
+  expect(tree).toMatchSnapshot();
+});
+
