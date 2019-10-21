eb6c31adb554db4a27c3efd4f0e6de5f831d5471
nullorone <sy.v@mail.ru>
2019-10-21 23:42:57 +0300

    Добавляет тесты для пропсов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Welcome} from '../welcome/welcome';
 import PropTypes from 'prop-types';
 
-const {number} = PropTypes;
+const {string, number, arrayOf, shape, oneOf} = PropTypes;
 
 const App = (props) => {
   const {time, mistakes} = props;
@@ -18,6 +18,39 @@ const App = (props) => {
 App.propTypes = {
   time: number.isRequired,
   mistakes: number.isRequired,
+  questions: arrayOf(
+      shape(
+          {
+            type: oneOf([`genre`, `artist`]),
+            genre: oneOf([`rock`, `jazz`, `pop`]),
+            answers: arrayOf(
+                shape(
+                    {
+                      src: string,
+                      genre: oneOf([`rock`, `jazz`, `pop`])
+                    }
+                )
+            ),
+          }
+      ),
+      shape(
+          {
+            type: oneOf([`genre`, `artist`]),
+            song: shape({
+              artist: oneOf([`Jim Beam`, `John Snow`, `Jack Daniels`]),
+              src: string,
+            }),
+            answers: arrayOf(
+                shape(
+                    {
+                      picture: string,
+                      artist: oneOf([`Jim Beam`, `John Snow`, `Jack Daniels`])
+                    }
+                )
+            ),
+          }
+      )
+  ),
 };
 
 export {App};
