1766f600cabc90a7000ab16b390f041850c0e2e2
nullorone <sy.v@mail.ru>
2019-10-21 23:42:34 +0300

    Использует моки при инициализации

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app/app';
 import {GAME_TIMES, AMOUNT_MISTAKES} from './mocks/data';
+import {questions} from './mocks/questions';
 import {getRandomNumber} from "./util";
 
 const randomGameTime = getRandomNumber(0, GAME_TIMES.length - 1);
 const randomAmountMistakes = getRandomNumber(0, AMOUNT_MISTAKES.length - 1);
 
-const init = () => {
+const init = (gameQuestions) => {
   ReactDOM.render(
       <App
         time={GAME_TIMES[randomGameTime]}
         mistakes={AMOUNT_MISTAKES[randomAmountMistakes]}
+        questions={gameQuestions}
       />,
       document.querySelector(`#root`)
   );
 };
 
-init();
+init(questions);
