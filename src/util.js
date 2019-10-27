7005a41e5320a43dc27044d5f2ba0cf7f4840858
nullorone <sy.v@mail.ru>
2019-10-27 14:45:24 +0300

    Добавляет функцию обработки формата времени

--- src/util.js
+++ src/util.js
@@ -5,4 +5,12 @@ const getRandomNumber = (beginNumber = 0, endNumber) => {
   return Math.floor(Math.random() * (endNumber - beginNumber + 1)) + beginNumber;
 };
 
-export {getRandomNumber};
+// Получаем формат числа 0n
+const checkTimeFormat = (questionTime) => {
+  return questionTime < 10 ? `0${questionTime}` : questionTime;
+};
+
+export {
+  getRandomNumber,
+  checkTimeFormat,
+};
