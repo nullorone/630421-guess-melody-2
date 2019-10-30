bd1c502f5128176586e3f5dc6ecb06cc55ca4b0e
nullorone <sy.v@mail.ru>
2019-10-30 23:45:23 +0300

    Перемещает константы

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,16 @@ const Value = {
 
 const EMPTY_STRING = ``;
 
+const GAME_TIMES = [5, 8, 10];
+
+const AMOUNT_MISTAKES = [1, 3, 5];
+
+const MOCK_ARTISTS = [`Пелагея`, `Краснознаменная дивизия имени моей бабушки`, `Lorde`];
+
 export {
   Value,
-  EMPTY_STRING
+  EMPTY_STRING,
+  GAME_TIMES,
+  AMOUNT_MISTAKES,
+  MOCK_ARTISTS
 };
--- src/mocks/data.js
+++
@@ -1,6 +0,0 @@
-// Моковые данные
-
-export const GAME_TIMES = [5, 8, 10];
-
-export const AMOUNT_MISTAKES = [1, 3, 5];
-
